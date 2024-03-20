ec16db66e2ee4742f1d6a616956144e35cd7cab3
ako gabriel <akog@mymacewan.ca>
2024-03-19 19:12:15 -0600

    Add project files.

---
+++ Akog_Assignment2/NumberChecker.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Akog_Assignment2
+{
+    internal class NumberChecker
+    {
+
+        public bool IsEven(int number)
+        {
+            if (number % 2 == 0)
+            {
+
+                return true; 
+            }
+            else
+            {
+
+                return false;  
+            }
+        }
+    }
+}
---
+++ Akog_Assignment2/Program.cs
@@ -0,0 +1,37 @@
+﻿using Akog_Assignment2;
+
+namespace EvenOddSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Initialize variables to store sums
+            int evenSum = 0;
+            int oddSum = 0;
+
+            // Create NumberChecker object
+            var numberEven = new NumberChecker();
+
+
+            for (int num = 0; num < 10; num++)
+            {
+                if (numberEven.IsEven(num))
+                {
+                    Console.WriteLine($"{num} is an even number.");
+                    evenSum += num;
+                }
+                else
+                {
+                    Console.WriteLine($"{num} is an odd number.");
+                    oddSum += num;
+                }
+            }
+
+
+            Console.WriteLine($"Total of even numbers : {evenSum}");
+            Console.WriteLine($"Total of odd numbers : {oddSum}");
+        }
+    }
+}
